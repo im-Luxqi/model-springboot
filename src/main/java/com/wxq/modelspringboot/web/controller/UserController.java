8a89db52afe95c884e869bc0d454c6d72686b76c
星齐 <Im_Luxqi@163.com>
2018-12-28 09:22:32 +0800

    canvas

--- src/main/java/com/wxq/modelspringboot/web/controller/UserController.java
+++ src/main/java/com/wxq/modelspringboot/web/controller/UserController.java
@@ -15,15 +15,51 @@ public class UserController {
         model.addAttribute("username", principal.getName());
         return "page_dashboard/dashboard";
     }
-    @GetMapping("/user/dashboard2")
-    public String user2(Model model){
-//        model.addAttribute("username", principal.getName());
-        return "page_dashboard/dashboard2";
-    }
 
     @GetMapping("/user/canvasText")
-    public String canvasText(Model model){
+    public String canvasText(){
         return "page_canvas/canvas_text";
+    }
+
+    @GetMapping("/user/canvasGameFiveChess")
+    public String canvasGameFiveChess(){
+        return "page_canvas/canvas_game_fiveChess";
+    }
+
+    @GetMapping("/user/canvasGameTank")
+    public String canvasGameTank(){
+        return "page_canvas/canvas_game_tank";
+    }
+
+    @GetMapping("/user/canvasGameSnake")
+    public String canvasGameSnake(){
+        return "page_canvas/canvas_game_snake";
+    }
+
+    @GetMapping("/user/canvasSmallBall")
+    public String canvasSamllBall(){
+        return "page_canvas/canvas_smallBall";
+    }
+    @GetMapping("/user/canvasHeart")
+    public String canvasHeart(){
+        return "page_canvas/canvas_heart";
+    }
+    @GetMapping("/user/canvasLoveTime")
+    public String canvasLoveTime(){
+        return "page_canvas/canvas_loveTime";
+    }
+    @GetMapping("/user/jasper")
+    public String jasper(){
+        return "page_jasper/jasper";
+    }
+
+
+
+
+
+
+    public void print(){
+
     }
 
 }
--- src/main/resources/templates/static/main_menu.js
+++ src/main/resources/templates/static/main_menu.js
@@ -1,12 +1,46 @@
 $(document).ready(function () {
 
     $("#photo").on("click",function () {
-        $("#menuForm").attr("action","/user/dashboard2")
+        $("#menuForm").attr("action","/user/dashboard")
         $("#menuForm").submit();
     });
 
     $("#canvas-text").on("click",function () {
         $("#menuForm").attr("action","/user/canvasText")
+        $("#menuForm").submit();
+    });
+
+    $("#canvas-game-fiveChess").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasGameFiveChess")
+        $("#menuForm").submit();
+    });
+
+    $("#canvas-game-tank").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasGameTank")
+        $("#menuForm").submit();
+    });
+    $("#canvas-game-snake").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasGameSnake")
+        $("#menuForm").submit();
+    });
+
+    $("#canvas-smallBall").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasSmallBall")
+        $("#menuForm").submit();
+    });
+
+    $("#canvas-heart").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasHeart")
+        $("#menuForm").submit();
+    });
+
+    $("#canvas-loveTime").on("click",function () {
+        $("#menuForm").attr("action","/user/canvasLoveTime")
+        $("#menuForm").submit();
+    });
+
+    $("#jasper").on("click",function () {
+        $("#menuForm").attr("action","/user/jasper")
         $("#menuForm").submit();
     });
 
